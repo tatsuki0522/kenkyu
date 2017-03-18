228725429b4ecb2af00a1bbcc2bb838cec4da6b5
tatsuki <tatsuki@DESKTOP-C825U7Q>
2017-03-18 19:36:39 +0900

    様々なエラーを改善

--- Win32Project1/Win32Project1/main.cpp
+++ Win32Project1/Win32Project1/main.cpp
@@ -2,10 +2,15 @@
 //#include "MT.h"
 //#include "main.h"
 //#include "define.h"
-
 #include "DxLib.h"
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
+void Game()
+{
+
+}
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
 	DxLib_Init();	// DXライブラリ初期化処理
 	WaitKey();	// キー入力待ち
 	DxLib_End();	// DXライブラリ終了処理

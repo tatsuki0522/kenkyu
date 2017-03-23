6f35773278b2895fede444a903cfb08b5e1a74b0
s1519x <s1519x@NETPC15>
2017-03-23 14:46:40 +0900

    ボタンの仕様

--- Win32Project1/Win32Project1/main.cpp
+++ Win32Project1/Win32Project1/main.cpp
@@ -5,25 +5,48 @@
 #include "DxLib.h"
 //#include "dxlib\DxLib.h"
 
+int boll_midium_graph;
 int boll_small_graph;
+int mx, my;
 
-void Draw()
+void Draw(struct Button_t *button)
 {
+	
+	DrawFormatString(20, 0, GetColor(255, 255, 255), "x=%d,y=%d", mx, my);
+	DrawFormatString(0, 20, GetColor(255, 255, 255), "up=%d,down=%d",button->up,button->down);
+	DrawLine(0, 0, 0, 500, GetColor(255, 255, 255));
+	DrawLine(380, 0,380, 500, GetColor(255, 255, 255));
+	DrawRotaGraph(288, 400, 1, 0, boll_midium_graph, 1, 0);
 	DrawRotaGraph(200, 200, 1, 0, boll_small_graph, 1, 0);
 }
 
-void Game()
+void Button(struct Button_t *button)
+{
+	GetMousePoint(&mx, &my);
+	if (CheckHitKey(KEY_INPUT_UP) == TRUE)button->up = TRUE;
+	else button->up = FALSE;
+	if (CheckHitKey(KEY_INPUT_DOWN) == TRUE)button->down = TRUE;
+	else button->down = FALSE;
+	if (CheckHitKey(KEY_INPUT_RIGHT) == TRUE)button->right = TRUE;
+	else button->right = FALSE;
+	if (CheckHitKey(KEY_INPUT_LEFT) == TRUE)button->left = TRUE;
+	else button->left = TRUE;
+}
+
+void Game(struct Button_t *button)
 {
 	while (1) {
 		ProcessMessage();	//プロセスの連絡
 		ClearDrawScreen();	//画面の削除
-		Draw();				//絵画をすべて処理
+		Draw(button);				//絵画をすべて処理
+		Button(button);
 		ScreenFlip();		//裏画面を表画面に反映
 	}
 }
 
 void Load()
 {
+	boll_midium_graph = LoadGraph(BOLL_MIDIUM_GRAPH);
 	boll_small_graph = LoadGraph(BOLL_SMALL_GRAPH);
 }
 
@@ -35,13 +58,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	struct Mob_t *mob;
 	struct Boll_t * bools;
 	struct Myself_t *mys;
+	struct Button_t *button;
+	button = (Button_t *)malloc(sizeof(Button_t)*MAX_BALL);
 	mob = (Mob_t *)malloc(sizeof(mob)*MAX_MOB);
 	bools = (Boll_t *)malloc(sizeof(Boll_t)*MAX_BALL);
 	mys = (Myself_t *)malloc(sizeof(Myself_t));
 	////////ファイルの読み込み///////
 	Load();
 	///////関数への処理渡し//////
-	Game();
+	Game(button);
 	///////終了命令////////
 	free(mob);
 	free(bools);

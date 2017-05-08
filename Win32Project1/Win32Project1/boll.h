aa8780118a3665cfcc5c65401246003980541c3c
s1536b@machidath.ed.jp <s1536b@machidath.ed.jp>
2017-05-08 11:08:54 +0900

    オブジェクトファイルの重複問題を

--- Win32Project1/Win32Project1/boll.h
+++ Win32Project1/Win32Project1/boll.h
@@ -1,3 +1,2 @@
 #pragma once
-
-void Boll_main(struct Boll_t *boll);
+void Boll_main();
--- Win32Project1/Win32Project1/boll_src.cpp
+++ Win32Project1/Win32Project1/boll_src.cpp
@@ -1,24 +1,10 @@
-
+#include "boll.h"
 #include"main.h"
 #include"define.h"
 #include<DxLib.h>
 
-void Boll_main(struct Boll_t *boll){
+void Boll_main(){
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

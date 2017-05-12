d66eb09b506825cdc7a6ee3338f213c73c8b9cdd
mori <0000>
2017-05-12 15:51:48 +0900

    boll_src.cpp

--- Win32Project1/Win32Project1/boll_src.cpp
+++ Win32Project1/Win32Project1/boll_src.cpp
@@ -8,19 +8,3 @@ void Boll_main(struct Boll_t *boll){
 }
 
 
-void boss_shot_bulletH005(){
-    #define TM005 820
-    #define RANGE005 100.0
-    #define LEM005 4
-	int i, j, k, n, t = boss_shot.cnt% TM005.t2 = boss_shot.cnt;
-	static int sst,bnum;
-	static double sx, sy, sangle;
-
-	if (t == 0){
-		sst = 0;
-		sx = boss.x;
-		sy = boss.y - RANGE005;
-		sangle = PI / 5 / 2 + PI / 2;
-		bnum = 0;
-	}
-}
